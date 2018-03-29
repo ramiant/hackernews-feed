c2f0c6a839abcf220cec710a66752fd43c47e400
Marian <tarlungeanu.marian95@gmail.com>
2018-03-29 18:24:55 +0300

    Added mock style

--- src/components/newsfeed/post/view.js
+++ src/components/newsfeed/post/view.js
@@ -8,15 +8,15 @@ export default class Post extends Component {
     render() {
         return (
             <div 
-                className={`Post ${this.store.getPost(this.props.id).isVisited ? 'Post--Visited' : ''}`} 
+                className={`Post ${this.store.getPost(this.props.id).isVisited ? 'Post--Visited' : ''} ${this.store.getPost(this.props.id).type === "mock" ? 'Post--Mock' : ''}`} 
                 onClick={this.store.getPost(this.props.id).type !== "mock" ? this._onClick.bind(this) : null}
             >
                 <div className={`Post__Favicon ${this.store.getPost(this.props.id).type === "mock" ? 'hidden' : ''}`}>
                     <span>&#x25C9;</span>
                     <img 
                         src={this.props.url ? "https://www.google.com/s2/favicons?domain_url=" + new URL(this.props.url).hostname : "http://via.placeholder.com/30x30"}
-                        width={30} 
-                        height={30} 
+                        width={35} 
+                        height={35} 
                         alt={"favicon?"+this.props.id}></img>
                 </div>
                 <div className="Post__Content">
@@ -29,7 +29,7 @@ export default class Post extends Component {
                         </div>
                     </div>
                     <div className="Post__Score">
-                        {this.props.score}
+                        <big>{this.props.score}</big>
                     </div>
                 </div>
             </div>
